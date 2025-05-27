4913cc7acd317b0dcdca51793782e7c9e982e5c6
firedcto@gmail.com <firedcto@gmail.com>
2025-05-27 21:43:25 +0800

    修复观战无奖励的时候无法继续推图

--- src/tasks/ClearMapTask.py
+++ src/tasks/ClearMapTask.py
@@ -36,7 +36,7 @@ class ClearMapTask(BaseGfTask):
                 return
             if boxes := self.wait_ocr(box='right', match=["特殊奖励", '观看', '挑战'], time_out=3, log=True):
                 # self.log_debug(f'特殊奖励 {boxes}')
-                if self.find_boxes(boxes, match=["特殊奖励"]):
+                if self.find_boxes(boxes, match=["特殊奖励", '观看']):
                     text = self.find_boxes(boxes, match=['观看', '挑战'])
                     self.click(text)
                     count += 1
