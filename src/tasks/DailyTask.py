e3a086d1c1b0ada2c568b43e5a9fda5d8723b29a
ok-oldking <154002444+ok-oldking@users.noreply.github.com>
2025-05-31 18:14:00 +0800

    修复公共区不点派遣

--- src/tasks/DailyTask.py
+++ src/tasks/DailyTask.py
@@ -126,15 +126,16 @@ class DailyTask(BaseGfTask):
     def gongongqu(self):
         self.info_set('current_task', 'public area')
         self.wait_click_ocr(match=['公共区'], box='right', after_sleep=0.5, raise_if_not_found=True)
-        self.wait_click_ocr(match=['调度室'], box='left', after_sleep=0.5, raise_if_not_found=True)
+        self.wait_click_ocr(match=['调度室'], box='left', settle_time=0.5, after_sleep=0.5, raise_if_not_found=True)
         self.wait_click_ocr(match=['调度收益'], box='bottom', after_sleep=0.5, raise_if_not_found=True)
         self.wait_click_ocr(match=['取出'], box='bottom', after_sleep=0.5, raise_if_not_found=True)
         self.wait_click_ocr(match=['资源生产'], box='left', after_sleep=0.5, raise_if_not_found=True)
         self.wait_click_ocr(match=['收取'], box='bottom', after_sleep=0.5, raise_if_not_found=True)
-        self.wait_click_ocr(match=pop_ups, box='bottom', after_sleep=0.5, raise_if_not_found=False)
+        self.wait_pop_up(count=1)
         self.back()
         self.sleep(1)
-        if self.wait_click_ocr(match=['一键领取', '领取全部'], box='bottom', after_sleep=1, time_out=5):
+        if self.wait_click_ocr(match=['一键领取', '领取全部'], settle_time=0.5, box='bottom_right', after_sleep=1,
+                               time_out=5):
             self.wait_click_ocr(match=['再次派遣'], box='bottom', after_sleep=1, raise_if_not_found=False)
         self.back()
         self.ensure_main()
